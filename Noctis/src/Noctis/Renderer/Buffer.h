68e7232f57148466e2b5309d6cda198976aaa8d1
Bjornakriin <baumer.eli@gmail.com>
2019-07-11 20:15:25 +0100

    Vertex Buffers

--- Noctis/src/Noctis/Renderer/Buffer.h
+++ Noctis/src/Noctis/Renderer/Buffer.h
@@ -1,6 +1,86 @@
 #pragma once
 
 namespace Noctis {
+	enum class ShaderDataType {
+		None = 0, Float, Float2, Float3, Float4, Int, Int2, Int3, Int4, Mat3, Mat4, Bool 
+	};
+
+	static uint32_t ShaderDataTypeSize(ShaderDataType type) {
+		switch (type) {
+			case ShaderDataType::Float:  return 4;
+			case ShaderDataType::Float2: return 4 * 2;
+			case ShaderDataType::Float3: return 4 * 3;
+			case ShaderDataType::Float4: return 4 * 4;
+			case ShaderDataType::Int:    return 4;
+			case ShaderDataType::Int2:   return 4 * 2;
+			case ShaderDataType::Int3:   return 4 * 3;
+			case ShaderDataType::Int4:   return 4 * 4;
+			case ShaderDataType::Mat3:   return 4 * 3 * 3;
+			case ShaderDataType::Mat4:   return 4 * 4 * 4;
+			case ShaderDataType::Bool:   return 1 ;
+		}
+		NT_CORE_ASSERT(false, "SHader Data Type Unknown!");
+		return 0;
+	}
+
+	struct BufferElement {
+		std::string m_Name;
+		ShaderDataType m_Type;
+		uint32_t m_Size;
+		uint32_t m_Offset;
+
+		BufferElement(const ShaderDataType& type, const std::string& name) 
+			: m_Type(type), m_Name(name), m_Size(ShaderDataTypeSize(type)), m_Offset(0)
+		{
+		};
+
+		uint32_t GetComponentCount() const {
+			switch (m_Type) {
+				case ShaderDataType::Float:  return 1;
+				case ShaderDataType::Float2: return 2;
+				case ShaderDataType::Float3: return 3;
+				case ShaderDataType::Float4: return 4;
+				case ShaderDataType::Int:    return 1;
+				case ShaderDataType::Int2:   return 2;
+				case ShaderDataType::Int3:   return 3;
+				case ShaderDataType::Int4:   return 4;
+				case ShaderDataType::Mat3:   return 3 * 3;
+				case ShaderDataType::Mat4:   return 4 * 4;
+				case ShaderDataType::Bool:   return 1;
+			}
+			NT_CORE_ASSERT(false, "Shader Data Type Unknown!");
+			return 0;
+			};
+		};
+	};
+
+	class BufferLayout {
+	public:
+		BufferLayout(const std::initializer_list<BufferElement>& elements) 
+			: m_Elements(elements) 
+		{
+			CalculateOffsetsAndStride();
+		};
+
+		inline const std::vector<BufferElement>& GetElements() {return m_Elements;}
+		
+		std::vector<BufferElement>::iterator begin() { return m_Elements.begin(); };
+		std::vector<BufferElement>::iterator end() { return m_Elements.end(); };
+	private:
+		void CalculateOffsetsAndStride() {
+			uint32_t offset = 0;
+			m_Stride = 0;
+			for (auto& element : m_Elements) {
+				element.m_Offset = offset;
+				offset += element.m_Size;
+				m_Stride += element.m_Size;
+			}
+		}
+	private:
+		std::vector<BufferElement> m_Elements;
+		uint32_t m_Stride = 0;
+	};
+
 	class VertexBuffer {
 	public:
 		virtual ~VertexBuffer() {};
@@ -8,8 +88,12 @@ namespace Noctis {
 		virtual void Bind() const = 0;
 		virtual void Unbind() const = 0;
 
+		virtual const BufferLayout& GetLayout() const = 0;
+		virtual void SetLayout(const BufferLayout& layout) = 0;
+
 		static VertexBuffer* Create(float* vertices, uint32_t size);
 	};
+
 	class IndexBuffer {
 	public:
 		virtual ~IndexBuffer() {};
--- Noctis/src/Platform/OpenGL/OpenGLBuffer.h
+++ Noctis/src/Platform/OpenGL/OpenGLBuffer.h
@@ -9,11 +9,14 @@ namespace Noctis {
 
 		virtual ~OpenGLVertexBuffer();
 
-		virtual void Bind() const ;
-		virtual void Unbind() const ;
+		virtual void Bind() const override ;
+		virtual void Unbind() const override;
 
+		virtual const BufferLayout& GetLayout() const override { return m_Layout; };
+		virtual void SetLayout(const BufferLayout& layout) override { m_Layout = layout; };
 	private:
 		uint32_t m_RendererID;
+		BufferLayout m_Layout;
 	};
 
 	class OpenGLIndexBuffer : public IndexBuffer {
